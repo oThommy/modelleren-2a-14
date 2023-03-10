49c55cf251bb4f8fc58695e793dab0eeb37ff223
noa2309 <125479016+noa2309@users.noreply.github.com>
2023-03-10 15:17:30 +0100

    Add files via upload

--- src/reproduce figures.py
+++ src/reproduce figures.py
@@ -41,6 +41,7 @@ plt.xlabel(r'Capacity $C_{t,i}$ $(Ah)$')
 plt.ylabel(r'$RUL_{t,i}$ (Cycles)')
 plt.show()
 
+
 ##Figure 3
 b = pd.read_csv("Battery_test.csv")
 exclude = ["Cycle"]
@@ -51,8 +52,32 @@ plt.ylabel(r'Capacity $C_{t,i}$ $(Ah)$')
 plt.show()
 
 
+##Figure 4
+T2 = []
+for i in range(1,11):
+    for t in range(1696):
+        if str(b.iloc[t,i]) == "nan":
+            break
+    T2+=[t+1]
+
+RUL2 = []
+for i in T2:
+    temp = []
+    for j in range(i):
+        temp += [i-j]
+    RUL2 += [temp]
+
+CAP2 = []
+for i in range(len(T2)):
+    temp2 = []
+    for j in range(T2[i]):
+        temp2 += [b.iloc[j,i+1]]
+    CAP2 += [temp2]
 
 
-
-
-
+plt.figure()
+for k in range(10):
+    plt.plot(CAP2[k], RUL2[k])
+plt.xlabel(r'Capacity $C_{t,i}$ $(Ah)$')
+plt.ylabel(r'$RUL_{t,i}$ (Cycles)')
+plt.show()
